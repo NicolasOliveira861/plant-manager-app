252532fd45f5d56c5013e6f51aa5c142fac38c22
Nícolas Oliveira <45276126+NicolasOliveira861@users.noreply.github.com>
2021-04-26 15:21:54 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.6:3333",
+  baseURL:
+    "https://my-json-server.typicode.com/NicolasOliveira861/plant-manager-app",
 });
 
 export default api;
